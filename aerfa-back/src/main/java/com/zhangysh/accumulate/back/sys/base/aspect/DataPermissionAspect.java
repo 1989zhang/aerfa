e2edca8c689dcb0d3058194ac26703a845a5ef71
1989zhang <215642240@qq.com>
2020-02-17 10:56:42 +0800

    20200217

--- aerfa-back/src/main/java/com/zhangysh/accumulate/back/sys/base/aspect/DataPermissionAspect.java
+++ aerfa-back/src/main/java/com/zhangysh/accumulate/back/sys/base/aspect/DataPermissionAspect.java
@@ -3,9 +3,11 @@ package com.zhangysh.accumulate.back.sys.base.aspect;
 import com.alibaba.fastjson.JSON;
 import com.zhangysh.accumulate.back.support.service.IRedisRelatedService;
 import com.zhangysh.accumulate.back.sys.base.aspect.annotation.DataPermission;
+import com.zhangysh.accumulate.back.sys.service.IDataPermissionService;
 import com.zhangysh.accumulate.common.constant.CacheConstant;
 import com.zhangysh.accumulate.common.util.HttpStorageUtil;
 import com.zhangysh.accumulate.common.util.StringUtil;
+import com.zhangysh.accumulate.pojo.base.BaseDataObj;
 import com.zhangysh.accumulate.pojo.sys.dataobj.AefsysPerson;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -13,7 +15,10 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -37,11 +42,16 @@ public class DataPermissionAspect {
 
     @Autowired
     private IRedisRelatedService redisRelatedService;
-
+    @Autowired
+    private IDataPermissionService dataPermissionService;
     /**
      * 数据权限过滤关键字
      */
     public static final String DATA_SCOPE = "dataPermission";
+    //过滤反射对象属性serialVersionUID
+    public static final String SERIAL_VERSION_UID = "serialVersionUID";
+    //过滤反射对象属性pageInfo
+    public static final String PAGE_INFO = "pageInfo";
 
     // 配置织入点
     @Pointcut("@annotation(com.zhangysh.accumulate.back.sys.base.aspect.annotation.DataPermission)")
@@ -90,9 +100,41 @@ public class DataPermissionAspect {
      * @param joinPoint 切入点目标
      */
     public static void dataPermissionFilter(JoinPoint joinPoint, AefsysPerson currentPerson, String tableNameIdentify){
-        logger.info(joinPoint.toString());
-        logger.info(currentPerson.toString());
-        logger.info(tableNameIdentify.toString());
+        try {
+            logger.info(joinPoint.toString());
+            logger.info(currentPerson.toString());
+            logger.info(tableNameIdentify.toString());
+            //组装追加的sql,要和权限控制一起完成
+            //dataPermissionService.listDataPermission()
+
+            String dataPermissionSql="1=1";
+            //配置追加的sql参数
+            Object[] argsObj = joinPoint.getArgs();
+            for (Object obj : argsObj) {
+                if (!(obj instanceof HttpServletRequest)) {
+                    //通过属性反射得到其查询对象
+                    Field[] fields = obj.getClass().getDeclaredFields();
+                    for (Field field : fields) {
+                        String name = field.getName();
+                        //非serial和分页对象即为查询对象
+                        if(!SERIAL_VERSION_UID.equals(name)&&!PAGE_INFO.equals(name)){
+                            field.setAccessible(true); // 私有属性必须设置访问权限
+                            Object resultValue = field.get(obj);
+                            BaseDataObj baseDataObj=(BaseDataObj)resultValue;
+                            Map<String, Object> paramsMap=baseDataObj.getParams();
+                            //参数有可能为null要报异常，当为空时创建个查询对象
+                            if(paramsMap==null){
+                                paramsMap=new HashMap<String, Object>();
+                                baseDataObj.setParams(paramsMap);
+                            }
+                            baseDataObj.getParams().put(DATA_SCOPE,dataPermissionSql);
+                        }
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     /**
