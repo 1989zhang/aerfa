32edcc561a103e88faa1cd6f140dfcbbfeac83d4
1989zhang <215642240@qq.com>
2020-02-18 23:27:21 +0800

    20200217

--- aerfa-ui/src/main/java/com/zhangysh/accumulate/ui/plugins/shiro/UserRealm.java
+++ aerfa-ui/src/main/java/com/zhangysh/accumulate/ui/plugins/shiro/UserRealm.java
@@ -1,11 +1,29 @@
 package com.zhangysh.accumulate.ui.plugins.shiro;
-
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.zhangysh.accumulate.common.constant.CacheConstant;
+import com.zhangysh.accumulate.common.constant.MarkConstant;
+import com.zhangysh.accumulate.common.constant.SysDefineConstant;
+import com.zhangysh.accumulate.common.pojo.TokenModel;
+import com.zhangysh.accumulate.common.util.IpUtil;
+import com.zhangysh.accumulate.pojo.sys.dataobj.AefsysResource;
+import com.zhangysh.accumulate.pojo.sys.dataobj.AefsysRole;
+import com.zhangysh.accumulate.pojo.sys.transobj.AefsysLoginDto;
+import com.zhangysh.accumulate.pojo.sys.viewobj.AefsysPersonVo;
+import com.zhangysh.accumulate.ui.sys.util.ServletUtil;
+import com.zhangysh.accumulate.ui.sys.service.ILoginService;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Resource;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 自定义Realm 处理登录 权限
@@ -15,12 +33,45 @@ import org.apache.shiro.subject.PrincipalCollection;
  */
 public class UserRealm extends AuthorizingRealm {
 
+    private static final Logger logger= LoggerFactory.getLogger(UserRealm.class);
+
+    @Resource
+    private ILoginService loginService;
+
     /**
      * 资源角色授权逻辑
      */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        //给资源进行授权
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        String backToken=ShiroUtils.getToken();
+        String sessionInfoStr=loginService.getSessionByToken(backToken);
+        TokenModel tokenModel=JSON.parseObject(sessionInfoStr,TokenModel.class);
+        String roleListObjectJson =tokenModel.getSession().get(CacheConstant.TOKENMODEL_SESSION_KEY_ROLE)+"";
+        List<AefsysRole> roleList=JSON.parseObject(roleListObjectJson, List.class);
+        String resourceListObjectJson =tokenModel.getSession().get(CacheConstant.TOKENMODEL_SESSION_KEY_RESOURCE)+"";
+        List<AefsysResource> resourceList=JSON.parseObject(resourceListObjectJson, List.class);
+
+        // 角色加入AuthorizationInfo认证对象
+        Set<String> roleCode=new HashSet<String>();
+        for (AefsysRole role:roleList){
+            if(SysDefineConstant.DB_USEABLE_STATUS_VALID.equals(role.getStatus())) {
+                roleCode.add(role.getRoleCode());
+            }
+        }
+        info.setRoles(roleCode);
+
+        // 权限加入AuthorizationInfo认证对象
+        Set<String> stringPermissions=new HashSet<String>();
+        for(AefsysResource resource:resourceList){
+            if(SysDefineConstant.DB_USEABLE_STATUS_VALID.equals(resource.getStatus())){
+                stringPermissions.add(resource.getIdentify());
+            }
+        }
+
+        info.setStringPermissions(stringPermissions);
+        return info;
     }
 
     /**
@@ -28,6 +79,29 @@ public class UserRealm extends AuthorizingRealm {
      */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        return null;
+        UsernamePasswordToken usernamePassword = (UsernamePasswordToken) authenticationToken;
+        String username = usernamePassword.getUsername();
+        String password = "";
+        if (usernamePassword.getPassword() != null) {
+            password = new String(usernamePassword.getPassword());
+        }
+        AefsysLoginDto loginDto=new AefsysLoginDto();
+        loginDto.setAccount(username);
+        loginDto.setPassword(password);
+        loginDto.setClientIp(ServletUtil.getClientIpAddr());
+        loginDto.setServerIp(IpUtil.getHostIp());
+        loginDto.setBrowserType(ServletUtil.getClientBrowser());
+        loginDto.setOsType(ServletUtil.getClientOperatingSystem());
+
+        String loginInfoResultStr=loginService.checkLoginInfo(loginDto);
+        JSONObject loginInfoJson=JSON.parseObject(loginInfoResultStr);
+        if(!MarkConstant.MARK_RESULT_VO_SUCESS.equals(loginInfoJson.getInteger(MarkConstant.MARK_RESULT_VO_CODE))) {
+            logger.info("对用户[" + username + "]进行登录验证.验证未通过.");
+            throw new AuthenticationException("用户登录验证未通过");
+        }
+        //验证通过返回后台系统生成的token，前台系统并不保持用户对象，只保存其token
+        String backToken=loginInfoJson.getString(MarkConstant.MARK_RESULT_VO_DATA);
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(backToken, password, getName());
+        return info;
     }
 }
